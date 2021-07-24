7706e20f97475d3ba5e1c390617fed4ba1fba9c1
Nino chan <81684610+Nino-chan02@users.noreply.github.com>
2021-07-24 11:49:35 +0700

    Add files via upload

---
+++ message/help.js
@@ -0,0 +1,252 @@
+exports.gameMenu = (prefix) => {
+    return `
+┌──「 GAME-MENU 」
+│
+├ ❏ ${prefix}slot
+├ ❏ ${prefix}limitgame
+├ ❏ ${prefix}gelud @tag
+├ ❏ ${prefix}tictactoe @tag
+├ ❏ ${prefix}siapaaku
+├ ❏ ${prefix}family100
+├ ❏ ${prefix}kuismath
+├ ❏ ${prefix}asahotak
+├ ❏ ${prefix}tebaklirik
+├ ❏ ${prefix}tebaklagu
+├ ❏ ${prefix}tebakkata
+├ ❏ ${prefix}susunkata
+├ ❏ ${prefix}kimiakuis
+├ ❏ ${prefix}caklontong
+├ ❏ ${prefix}tebakjenaka
+├ ❏ ${prefix}tebakanime
+├ ❏ ${prefix}tebaktebakan
+├ ❏ ${prefix}tebakgambar
+├ ❏ ${prefix}tebakbendera
+├ ❏ ${prefix}suit *batu/kertas/gunting*
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.funMenu = (prefix) => {
+    return `
+┌──「 FUN-MENU 」
+│
+├ ❏ ${prefix}mining
+├ ❏ ${prefix}togel
+├ ❏ ${prefix}cekwatak
+├ ❏ ${prefix}cekmati [nama]
+├ ❏ ${prefix}wangy [nama]
+├ ❏ ${prefix}citacita
+├ ❏ ${prefix}toxic
+├ ❏ ${prefix}truth
+├ ❏ ${prefix}dare
+├ ❏ ${prefix}apakah
+├ ❏ ${prefix}bisakah
+├ ❏ ${prefix}kapankah
+├ ❏ ${prefix}rate
+├ ❏ ${prefix}jadian
+├ ❏ ${prefix}cantik
+├ ❏ ${prefix}ganteng
+├ ❏ ${prefix}beban
+├ ❏ ${prefix}babi
+├ ❏ ${prefix}cekganteng
+├ ❏ ${prefix}cekcantik
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.otherMenu = (prefix) => {
+    return `
+┌──「 OTHER-MENU 」
+│
+├ ❏ ${prefix}brainly *query*
+├ ❏ ${prefix}shopee *product*
+├ ❏ ${prefix}playstore *query*
+├ ❏ ${prefix}ssweb *query*
+├ ❏ ${prefix}google *query*
+├ ❏ ${prefix}image *query*
+├ ❏ ${prefix}pinterest *query*
+├ ❏ ${prefix}nulis *teks*
+├ ❏ ${prefix}iguser *ussername*
+├ ❏ ${prefix}igstalk *username*
+├ ❏ ${prefix}githubstalk *username*
+├ ❏ ${prefix}tiktokstalk *ussername*
+├ ❏ ${prefix}img2url *reply foto*
+├ ❏ ${prefix}ytsearch *query*
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.stickerMenu = (prefix) => {
+    return `
+┌──「 STICKER-MENU 」
+│
+├ ❏ ${prefix}dadu
+├ ❏ ${prefix}doge
+├ ❏ ${prefix}toimg
+├ ❏ ${prefix}patrick
+├ ❏ ${prefix}garwgura
+├ ❏ ${prefix}ttg *teks*
+├ ❏ ${prefix}attp *teks*
+├ ❏ ${prefix}stickeranime
+├ ❏ ${prefix}semoji *emoji*
+├ ❏ ${prefix}sticker *reply foto/video*
+├ ❏ ${prefix}smeme *teks|teks*
+├ ❏ ${prefix}swm *pack|author*
+├ ❏ ${prefix}take *pack|author* 
+├ ❏ ${prefix}tovideo *reply sgif*
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.wibuMenu = (prefix) => {
+    return `
+┌──「 WIBU-MENU 」
+│
+├ ❏ ${prefix}loli
+├ ❏ ${prefix}manga
+├ ❏ ${prefix}anime 
+├ ❏ ${prefix}lolivideo
+├ ❏ ${prefix}husbu
+├ ❏ ${prefix}waifu
+├ ❏ ${prefix}neko
+├ ❏ ${prefix}kanna
+├ ❏ ${prefix}sagiri
+├ ❏ ${prefix}hentai
+├ ❏ ${prefix}wallnime
+├ ❏ ${prefix}storyanime
+├ ❏ ${prefix}kusonime
+├ ❏ ${prefix}megumin
+├ ❏ ${prefix}nakanomiku
+├ ❏ ${prefix}nakanonino
+├ ❏ ${prefix}nakanoitsuki
+├ ❏ ${prefix}otakudesu
+├ ❏ ${prefix}doujindesu
+├ ❏ ${prefix}otakuongoing
+├ ❏ ${prefix}nhentai *code*
+├ ❏ ${prefix}nekopoi *link*
+├ ❏ ${prefix}nekopoi3d
+├ ❏ ${prefix}nekopoicosplay
+├ ❏ ${prefix}cosplayanime
+├ ❏ ${prefix}nekopoisearch
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.ownerMenu = (prefix) => {
+    return `
+┌──「 OWNER-MENU 」
+│
+├ ❏ ${prefix}bc *teks*
+├ ❏ ${prefix}term
+├ ❏ ${prefix}eval
+├ ❏ ${prefix}reset
+├ ❏ ${prefix}clearall
+├ ❏ ${prefix}join *link gc*
+├ ❏ ${prefix}shutdown
+├ ❏ ${prefix}getquoted
+├ ❏ ${prefix}addupdate *fiturnya*
+├ ❏ ${prefix}exif *nama|author*
+├ ❏ ${prefix}sewa add/del *waktunya*
+├ ❏ ${prefix}premium add @tag|nomor
+├ ❏ ${prefix}premium del @tag|nomor
+│
+└──「 *NINO BOT* 」
+`
+}
+
+exports.groupMenu = (prefix) => {
+    return`
+┌──「 GROUP-MENU 」
+│
+├ ❏ ${prefix}ceksewa
+├ ❏ ${prefix}group 
+├ ❏ ${prefix}afk *alasan*
+├ ❏ ${prefix}sider *reply chat bot*
+├ ❏ ${prefix}kickall
+├ ❏ ${prefix}infogrup
+├ ❏ ${prefix}promote
+├ ❏ ${prefix}demote
+├ ❏ ${prefix}listonline
+├ ❏ ${prefix}tagall *teks*
+├ ❏ ${prefix}leave
+├ ❏ ${prefix}mute
+├ ❏ ${prefix}unmute
+├ ❏ ${prefix}kick *reply*
+├ ❏ ${prefix}add *+62xxxxxx*
+├ ❏ ${prefix}setgrupname
+├ ❏ ${prefix}setppgrup
+├ ❏ ${prefix}setdesc
+├ ❏ ${prefix}hidetag *teks/reply teks*
+├ ❏ ${prefix}welcome *enable | disable*
+├ ❏ ${prefix}antilink *enable | disable*
+├ ❏ ${prefix}leveling *enable | disable*
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.downloadMenu = (prefix) => {
+    return `
+┌──「 DOWNLOAD-MENU 」
+│
+├ ❏ ${prefix}fb 
+├ ❏ ${prefix}igdl 
+├ ❏ ${prefix}igdl2 
+├ ❏ ${prefix}twitter 
+├ ❏ ${prefix}tiktok 
+├ ❏ ${prefix}play 
+├ ❏ ${prefix}ythd 
+├ ❏ ${prefix}ytmp3 
+├ ❏ ${prefix}ytmp4 
+├ ❏ ${prefix}soundcloud 
+├ ❏ ${prefix}tiktoknowm 
+├ ❏ ${prefix}tiktokaudio
+├ ❏ ${prefix}mediafire 
+├ ❏ ${prefix}nhentaipdf *code*
+│
+└──「 *NINO BOT* 」
+`
+}
+exports.rulesBot = (prefix) => {
+    return `
+-----[ RULES ]-----
+
+1. Jangan spam bot. 
+Sanksi: *WARN/SOFT BLOCK*
+
+2. Jangan telepon bot.
+Sanksi: *SOFT BLOCK*
+
+3. Jangan mengejek bot.
+Sanksi: *PERMANENT BLOCK*
+
+Jika sudah dipahami rules-nya, silakan ketik *#menu* untuk memulai!
+
+Owner Nino BOT:
+wa.me/6288286421519
+`
+}
+exports.infoMenu = (prefix) => {
+return `
+┌──「 INFO-MENU 」
+│
+├ ❏ ${prefix}update
+├ ❏ ${prefix}level
+├ ❏ ${prefix}rules
+├ ❏ ${prefix}profile
+├ ❏ ${prefix}waktu
+├ ❏ ${prefix}botstat
+├ ❏ ${prefix}sewabot
+├ ❏ ${prefix}listsewa
+├ ❏ ${prefix}owner
+├ ❏ ${prefix}ping
+├ ❏ ${prefix}runtime
+├ ❏ ${prefix}donasi
+├ ❏ ${prefix}leaderboard
+├ ❏ ${prefix}cekpremium
+├ ❏ ${prefix}listpremium
+├ ❏ ${prefix}sourcecode
+├ ❏ ${prefix}bugreport *keluhan*
+│
+└──「 *NINO BOT* 」
+`
+} 
